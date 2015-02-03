26e01f35c7f48b266260020fecaaae6e3928cecf
sevenxuy <sevenxuy@163.com>
2015-02-03 14:50:01 +0800

    clean code

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,7 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   livereload = require('gulp-livereload'),
-  // compass = require('gulp-compass'),
-  // base64 = require('gulp-base64'),
   jshint = require('gulp-jshint');
-
-/*gulp.task('compass', function() {
-  gulp.src('./src/css/*.scss')
-    .pipe(compass({
-      sass: './src/css',
-      image: './src/img',
-      relative: true,
-      comments: true
-    }))
-    .pipe(base64())
-    .pipe(gulp.dest('./src/css'))
-    .pipe(connect.reload());
-
-});*/
 
 gulp.task('jshint', function() {
   gulp.src('./src/js/app/*.js')
